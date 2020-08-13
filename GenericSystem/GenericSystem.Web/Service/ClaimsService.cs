70404b8f517338540103af8d1719060e68373568
lhmrodrigues <luishenrique.m.r.993@gmail.com>
2020-08-12 21:32:12 -0300

    [FEAT] Estrutura do projeto Web

---
+++ GenericSystem/GenericSystem.Web/Service/ClaimsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GenericSystem.Web.Service
+{
+    public static class ClaimsService
+    {
+        public static ClaimsPrincipal CreateClaimsPrincipal(IEnumerable<Claim> claims)
+        {
+            ClaimsIdentity identities = new ClaimsIdentity(claims, "Cookie");
+
+            return new ClaimsPrincipal(new[] { identities });
+        }
+
+        public static string ReadClaimsPrincipal(ClaimsPrincipal principal, string type)
+        {
+            return principal.Claims.SingleOrDefault(x => x.Type == type)?.Value;
+        }
+    }
+}
--- GenericSystem/GenericSystem.Web/Startup.cs
+++ GenericSystem/GenericSystem.Web/Startup.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using GenericSystem.Infra.CrossCutting.IoC;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StackExchange.Profiling.Storage;
 
 namespace GenericSystem.Web
 {
@@ -23,28 +29,69 @@ namespace GenericSystem.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddGenericSetup();
+
+            services.AddAntiforgery(options => options.HeaderName = "XSRF-TOKEN");
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                x.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(x =>
+            {
+                x.LoginPath = new PathString("/Login");
+                x.AccessDeniedPath = new PathString("/Negado");
+            });
+
+            services.AddMiniProfiler(options =>
+            {
+                // All of this is optional. You can simply call .AddMiniProfiler() for all defaults
+
+                // (Optional) Path to use for profiler URLs, default is /mini-profiler-resources
+                options.RouteBasePath = "/profiler";
+
+                // (Optional) Control storage
+                // (default is 30 minutes in MemoryCacheStorage)
+                (options.Storage as MemoryCacheStorage).CacheDuration = TimeSpan.FromMinutes(60);
+
+                // (Optional) You can disable "Connection Open()", "Connection Close()" (and async variant) tracking.
+                // (defaults to true, and connection opening/closing is tracked)
+                options.TrackConnectionOpenClose = true;
+            });
+
+            services.AddRazorPages().AddRazorPagesOptions(options =>
+            {
+                options.Conventions.AddPageRoute("/Login", "");
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
+                app.UseMiniProfiler();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseExceptionHandler("/Erro");
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
+            RequestLocalizationOptions localizationOptions = new RequestLocalizationOptions
+            {
+                SupportedCultures = new List<CultureInfo> { new CultureInfo("pt-BR") },
+                SupportedUICultures = new List<CultureInfo> { new CultureInfo("pt-BR") },
+                DefaultRequestCulture = new RequestCulture("pt-BR")
+            };
+
+            app.UseRequestLocalization(localizationOptions);
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
